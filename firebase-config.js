aedfb3476cf60a9b2e44d6727de4ea6d45fdf4f1
HELLFORGEINQUISITOR <adamlovell1616@gmail.com>
2025-02-23 23:42:36 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyCfanKgFO98R1uKPpEAUUFOW1U66knL8Zs",
+    authDomain: "hellforge-13d88.firebaseapp.com",
+    projectId: "hellforge-13d88",
+    storageBucket: "hellforge-13d88.firebasestorage.app",
+    messagingSenderId: "453743116508",
+    appId: "1:453743116508:web:8beaee82ff6b78a2167419",
+    measurementId: "G-WRCFGP7F79"
+};
