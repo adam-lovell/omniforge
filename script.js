3feaae7136e62d6519cb79638267269d72c6ebbf
HELLFORGEINQUISITOR <adamlovell1616@gmail.com>
2025-02-23 23:45:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
 import { getFirestore, collection, doc, getDoc, setDoc, addDoc, getDocs, deleteDoc, query, where } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
 
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCfanKgFO98R1uKPpEAUUFOW1U66knL8Zs",
-  authDomain: "hellforge-13d88.firebaseapp.com",
-  projectId: "hellforge-13d88",
-  storageBucket: "hellforge-13d88.firebasestorage.app",
-  messagingSenderId: "453743116508",
-  appId: "1:453743116508:web:8beaee82ff6b78a2167419",
-  measurementId: "G-WRCFGP7F79"
-};
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
