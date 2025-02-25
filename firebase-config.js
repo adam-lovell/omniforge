7ee4d6a89639c88832c466922a55cd7df2a49bf2
HELLFORGEINQUISITOR <adamlovell1616@gmail.com>
2025-02-24 22:20:05 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
-// firebase-config.js
-export const firebaseConfig = {
-    apiKey: "AIzaSyCfanKgFO98R1uKPpEAUUFOW1U66knL8Zs",
-    authDomain: "hellforge-13d88.firebaseapp.com",
-    projectId: "hellforge-13d88",
-    storageBucket: "hellforge-13d88.firebasestorage.app",
-    messagingSenderId: "453743116508",
-    appId: "1:453743116508:web:8beaee82ff6b78a2167419",
-    measurementId: "G-WRCFGP7F79"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCfanKgFO98R1uKPpEAUUFOW1U66knL8Zs",
+  authDomain: "hellforge-13d88.firebaseapp.com",
+  projectId: "hellforge-13d88",
+  storageBucket: "hellforge-13d88.firebasestorage.app",
+  messagingSenderId: "453743116508",
+  appId: "1:453743116508:web:8beaee82ff6b78a2167419",
+  measurementId: "G-WRCFGP7F79"
 };
